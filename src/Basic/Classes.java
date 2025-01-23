5723554eef6b0e0e076b82e4a3fa71813b2c50c3
Mahadevuni_Madhubabu <madhubabumahadevuni@gmail.com>
2025-01-23 23:02:18 +0530

    Basics Of OOPs

---
+++ src/Basic/Classes.java
@@ -0,0 +1,15 @@
+package Basic;
+
+public class Classes {
+
+    int data_member1;
+
+    Classes(){ //Default Constructer
+        data_member1 = 0;
+    }
+
+    Classes(int val){ //Custom constructer
+        data_member1 = val;
+    }
+}
+
