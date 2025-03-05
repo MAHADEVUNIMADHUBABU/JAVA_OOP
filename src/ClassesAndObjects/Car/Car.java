f271e6df7bae9420a82806b0af8b7d694d8e0098
Mahadevuni_Madhubabu <madhubabumahadevuni@gmail.com>
2025-03-05 23:35:29 +0530

    Classes

---
+++ src/ClassesAndObjects/Car/Car.java
@@ -0,0 +1,14 @@
+package ClassesAndObjects.Car;
+
+public class Car {
+
+    String engine;
+    int seats;
+    double price;
+
+    Car(String str , int s , double price){
+        engine = str;
+        seats = s;
+        this.price = price;
+    }
+}
---
+++ src/ClassesAndObjects/Car/Main.java
@@ -0,0 +1,9 @@
+package ClassesAndObjects.Car;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car maruthi = new Car("EnV1" , 5 , 200000.00);
+
+    }
+}
